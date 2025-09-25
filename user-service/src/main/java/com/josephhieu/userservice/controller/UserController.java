7103116156321a133d0b33de91b243206e7b16cf
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-09-25 21:24:48 +0700

    create user-service

---
+++ user-service/src/main/java/com/josephhieu/userservice/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.josephhieu.userservice.controller;
+
+
+import com.josephhieu.userservice.model.User;
+import com.josephhieu.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteUser(@PathVariable Long id) {
+        return userService.deleteUser(id);
+    }
+}
